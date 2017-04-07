527211907d310c62a23a545bc9e49ded87271314
Madalina Samoila <madalina.samoila@just-eat.com>
2017-04-07 11:00:32 +0100

    Adding changes from Dan + animations

---
+++ Assets/Completed/Scripts/Pigeon.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Completed;
+
+namespace Assets.Completed.Scripts
+{
+    class Pigeon : MovingObject
+    {
+        protected override void OnCantMove<T>(T component)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Assets/Completed/Scripts/PigeonMove.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System;
+using Assets.Completed.Scripts;
+using Completed;
+
+namespace Assets
+{
+    public class PigeonMove : MonoBehaviour
+    {
+        public GameObject pigeon;
+        public int lifetime;
+        public Vector2 direction;
+        public int speed;
+        public bool active;
+
+        public void Start()
+        {
+            pigeon = GameObject.FindGameObjectWithTag("Pigeon").GetComponent<GameObject>();
+        }
+
+        public void SpawnCar()
+        {
+            if (active)
+            {
+                GameObject pigeonClone = Pigeon.Instantiate(pigeon, this.transform.position, this.transform.rotation);
+                var pigeonCloneScript = pigeonClone.GetComponent<Pigeon>();
+                pigeonCloneScript.direction = direction;
+                pigeonCloneScript.lifetime = lifetime;
+                pigeonCloneScript.speed = speed;
+                pigeonClone.tag = "car";
+            }
+        }
+    }
+}
